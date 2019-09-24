7c968173bcc42c98017f41f14f7d9e4d265d7642
SupianIDz <supianidz@gmail.com>
2019-09-24 17:08:30 +0800

    Commit 17:08:07 Tue Sep 24 2019

---
+++ assets/js/autocomplete.js
@@ -0,0 +1,71 @@
+/**
+ *   ___       _
+ *  / _ \  ___| |_ ___  _ __  _   _
+ * | | | |/ __| __/ _ \| '_ \| | | |
+ * | |_| | (__| || (_) | |_) | |_| |
+ *  \___/ \___|\__\___/| .__/ \__, |
+ *                     |_|    |___/
+ * @author  : Supian M <supianidz@gmail.com>
+ * @link    : www.octopy.xyz
+ * @license : MIT
+ */
+
+function autocomplete(suggest) {
+    var input = $('input');
+    input.wrap('<span class="autocomplete" style="position: relative;"></span>');
+    var html =
+        '<span class="overflow" style="position: absolute; z-index: -10;">' +
+        '<span class="repeat" style="opacity: 0;"></span>' +
+        '<span class="guess"></span></span>';
+    $('.autocomplete').prepend(html);
+
+    // Search of input changes.
+    var repeat = $('.repeat');
+    var guess = $('.guess');
+    var search = function(command) {
+        var array = [];
+        for (var key in suggest) {
+            if (!suggest.hasOwnProperty(key))
+                continue;
+            var pattern = new RegExp(key);
+            if (command.match(pattern)) {
+                array = suggest[key];
+            }
+        }
+
+        var text = command.split(' ').pop();
+
+        var found = '';
+        if (text != '') {
+            for (var i = 0; i < array.length; i++) {
+                var value = array[i];
+                if (value.length > text.length &&
+                    value.substring(0, text.length) == text) {
+                    found = value.substring(text.length, value.length);
+                    break;
+                }
+            }
+        }
+        guess.text(found);
+    };
+    var update = function() {
+        var command = input.val();
+        repeat.text(command);
+        search(command);
+    };
+    input.change(update);
+    input.keyup(update);
+    input.keypress(update);
+    input.keydown(update);
+
+    input.keydown(function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 9) {
+            var val = input.val();
+            input.val(val + guess.text());
+            return false;
+        }
+    });
+
+    return input;
+}
--- assets/js/console.js
+++ assets/js/console.js
@@ -10,40 +10,51 @@
  * @license : MIT
  */
 
-class Code {
+class OctopyConsole {
     constructor() {
         this.str = '';
         this.int = -1;
     }
 
-    which(selector) {
-        $(selector).keydown(function(event) {
+    run(config) {
+        $('.prefix').html(
+            this.prefix = config.prefix.split('@').join('<font class="at">@</font>') + ' # '
+        );
+
+        this.selector = config.selector;
+        this.controller = config.controller;
+
+        $.getJSON(config.autocomplete, {}, function(json) {
+            Octopy.console.autocomplete(json);
+        });
+
+        $(Octopy.console.selector).keydown(function(event) {
             var input = $(this);
             if (event.which == 38) {
 
-                if (app.code.int == -1) {
-                    app.code.str = input.val();
+                if (Octopy.console.int == -1) {
+                    Octopy.console.str = input.val();
                 }
 
-                return input.val(app.code.get(++app.code.int));
+                return input.val(Octopy.console.get(++Octopy.console.int));
             } else if (event.which == 40) {
-                return input.val(app.code.get(--app.code.int));
+                return input.val(Octopy.console.get(--Octopy.console.int));
             } else if (event.which == 13) {
-                
-                app.code.set(input.val());
-               
+
+                Octopy.console.set(input.val());
+
                 if (input.val().toLowerCase() == 'clear') {
                     $('.output').html('');
                     input.val('');
                 } else {
-                    app.code.request(input.val());
+                    Octopy.console.request(input.val());
                 }
             }
         });
     }
 
     handle(data, command) {
-        var prefix = '<font class="prefix">' + this.bashrc + '</font>';
+        var prefix = '<font class="prefix">' + this.prefix + '</font>';
 
         $('.output').append(
             prefix + command + '<pre>' + data + '</pre>'
@@ -68,34 +79,91 @@ class Code {
     get(now) {
         var storage = localStorage['history'];
         var history = storage ? JSON.parse(storage) : [];
-       
+
         if (now < 0) {
-            app.code.int = now = -1;
-            return app.code.str;
+            Octopy.console.int = now = -1;
+            return Octopy.console.str;
         }
 
         if (now >= history.length) {
-            app.code.int = now = history.length - 1;
+            Octopy.console.int = now = history.length - 1;
         }
 
         return history[history.length - now - 1];
     }
 
     request(command) {
-        $.post('console.php', {
+        $.post(Octopy.console.controller, {
             command: command
         }, function(data) {
-            app.code.handle(data, command);
+            Octopy.console.handle(data, command);
         });
     }
 
-    prefix(prefix) {
-        this.bashrc = prefix.split('@').join('<font class="at">@</font>') + ' # ';
-        $('.prefix').html(this.bashrc);
-        return app.code;
+    autocomplete(suggest) {
+        var input = $(Octopy.console.selector);
+
+        input.wrap('<span class="autocomplete" style="position: relative;"></span>');
+
+        var html =
+            '<span class="overflow" style="position: absolute; z-index: -10;">' +
+            '<span class="repeat" style="opacity: 0;"></span>' +
+            '<span class="guess"></span></span>';
+        $('.autocomplete').prepend(html);
+
+        var guess = $('.guess');
+        var repeat = $('.repeat');
+        var search = function(command) {
+            var array = [];
+            for (var key in suggest) {
+                if (!suggest.hasOwnProperty(key))
+                    continue;
+                var pattern = new RegExp(key);
+                if (command.match(pattern)) {
+                    array = suggest[key];
+                }
+            }
+
+            var text = command.split(' ').pop();
+
+            var found = '';
+            if (text != '') {
+                for (var i = 0; i < array.length; i++) {
+                    var value = array[i];
+                    if (value.length > text.length &&
+                        value.substring(0, text.length) == text) {
+                        found = value.substring(text.length, value.length);
+                        break;
+                    }
+                }
+            }
+
+            guess.text(found);
+        };
+
+        var update = function() {
+            var command = input.val();
+            repeat.text(command);
+            search(command);
+        };
+
+        input.keyup(update);
+        input.change(update);
+        input.keydown(update);
+        input.keypress(update);
+
+        input.keydown(function(event) {
+            var code = (event.keyCode ? event.keyCode : event.which);
+            if (code == 9) {
+                input.val(input.val() + guess.text());
+                return false;
+            }
+        });
+
+        return input;
     }
 }
 
-var app = $.extend({}, jQuery, {
-    code: new Code()
+var Octopy = $.extend({}, jQuery, {
+    console: new OctopyConsole()
 });
